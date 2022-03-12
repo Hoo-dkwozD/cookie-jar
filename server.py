dd420fd7e01522db3b509fe5b14efd81a146d5b6
Derrick Hoon <derrickhoon@Derricks-Mac-Air-M1.local>
2022-03-12 12:37:31 +0800

    DB changes;

--- server.py
+++ server.py
@@ -1,20 +1,36 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+db = SQLAlchemy(app)
+
+class Cookie(db.Model):
+    __tablename__ = 'cookies'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    key = db.Column(db.String(64))
+    value = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.key}: {self.value}"
 
 @app.get("/")
 def index():
     data = request.cookies
 
-    with open("store.txt", "w") as file:
-        for key in data:
-            file.write(data[key] + "\n")
+    for key in data:
+        cookie = Cookie(key=key, value=data[key])
+
+        db.session.add(cookie)
+        db.session.commit()
     
-    with open("store.txt") as file:
-        results = file.read()
+    results = Cookie.query.all()
     
     return escape(results)
 
 if __name__ == "__main__":
+    db.create_all()
     app.run()
